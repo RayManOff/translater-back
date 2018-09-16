496f335eff3352733b1ae8bad9c707437e7eda45
rayman <raymanovg@gmail.com>
2018-09-16 16:41:13 +0300

    forming dialog content

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Slim\Container;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -12,15 +13,29 @@ $container = [
         'google' => [
             'access_token' => 'token'
         ]
-    ]
+    ],
 ];
 
-$container['logger'] = function(\Slim\Container $container) {
-    $logger = new \Monolog\Logger('my_logger');
+$container['logger'] = function(Container $container) {
+    $logger = new \Monolog\Logger('translater');
     $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
     $logger->pushHandler($file_handler);
 
     return $logger;
+};
+
+$container['twig'] = function (Container $container) {
+    $view = new \Slim\Views\Twig('templates', [
+//        'cache' => '../cache/templates'
+    ]);
+
+    /** @var Request $request */
+    $request = $container->get('request');
+
+    $basePath = rtrim(str_ireplace('index.php', '', $request->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
+
+    return $view;
 };
 
 $app = new \Slim\App($container);
@@ -29,13 +44,71 @@ $app = new \Slim\App($container);
 $logger = $app->getContainer()->get('logger');
 $logger->info('Starting app...');
 
-// Define app routes
-$app->post('/translate', function (Request $request, Response $response) {
+$app->get('/dialogContents', function (Request $request, Response $response) {
     $this->logger->info('Response...');
-    $feed = $request->getParsedBody();
 
-    return $response->withJson($feed);
+    /** @var \Slim\Views\Twig $twig */
+    $twig = $this->twig;
+    $result = [
+        'status' => true,
+        'dialogs' => [
+            'icon' => [
+                'tag' => 'div',
+                'attributes' => [
+                    'id' => 'custom_translate',
+                    'style' => [
+                        'width' => '19px',
+                        'height' => '19px',
+                        'border' => '1px solid black',
+                        'position' => 'absolute !important',
+                        'border-radius' => '5px'
+                    ]
+                ],
+                'content' => $twig->fetch('icon.html')
+            ],
+            'popup' => [
+                'tag' => 'div',
+                'attributes' => [
+                    'id' => 'translate_ext_rayman',
+                    'style' => [
+                        'width' => '100px',
+                        'height' => '100px',
+                        'border' => '1px solid black',
+                        'position' => 'absolute !important',
+                        'background-color' => 'white',
+                        'z-index' => 9999
+                    ]
+                ],
+            ]
+        ]
+    ];
+
+    /** @var \Slim\Views\Twig $twig */
+    return $response->withJson($result);
 });
 
-// Run app
+$app->post('/translate', function (Request $request, Response $response) {
+    $this->logger->info('Response...');
+    $result = [
+        'status' => true,
+        'translate_result' => []
+    ];
+
+    /** @var \Slim\Views\Twig $twig */
+    $twig = $this->twig;
+    $params = $request->getParsedBody();
+    if (empty($params['text'])) {
+        $result['status'] = false;
+        $result['error'] = 'There is not text to translate';
+    } else {
+        $result['data'] = [
+            'text' => 'Перевод текста " ' . $params['text'] . ' "',
+            'content' => $twig->fetch('popup.html', $params)
+        ];
+    }
+
+    /** @var \Slim\Views\Twig $twig */
+    return $response->withJson($result);
+});
+
 $app->run();
