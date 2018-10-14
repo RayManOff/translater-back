15d94cfced10225dc56d56245e6a0bf819ca342c
rayman <raymanovg@gmail.com>
2018-10-14 22:13:55 +0300

    + getIcon action

--- config/routes.php
+++ config/routes.php
@@ -4,12 +4,18 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 use App\Controller\Translate;
 
-$routes = new RouteCollection();
 $translateRoute = new Route('/translate', [
     '_controller' => [Translate::class, 'translate']
 ]);
-$translateRoute->setMethods(['POST']);
+$getIconRoute = new Route('/getIcon', [
+    '_controller' => [Translate::class, 'getIcon']
+]);
 
+$translateRoute->setMethods(['GET']);
+$translateRoute->setMethods(['GET']);
+
+$routes = new RouteCollection();
 $routes->add('translate', $translateRoute);
+$routes->add('getIcon', $getIconRoute);
 
 return $routes;
--- src/Controller/Translate.php
+++ src/Controller/Translate.php
@@ -6,6 +6,7 @@ use Stichoza\GoogleTranslate\TranslateClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @author Gadel Raymanov <raymanovg@gmail.com>
@@ -14,8 +15,11 @@ class Translate extends AbstractController
 {
     public function translate(Request $request)
     {
-        $text = $request->query->get('text', '');
-        $needPopup = $request->query->get('need_popup', false);
+        $text      = $request->query->get('text', '');
+        $needPopup = (bool) $request->query->get('need_popup', false);
+        $showPopup = (bool) $request->query->get('show_popup', false);
+        $source = $request->query->get('source', 'en');
+        $target = $request->query->get('source', 'ru');
 
         $response = [
             'success' => true,
@@ -27,22 +31,50 @@ class Translate extends AbstractController
         } else {
             try {
                 $client = new TranslateClient();
-                $client->setSource('en');
-                $client->setTarget('ru');
+                $client->setSource($source);
+                $client->setTarget($target);
                 $translate = $client->translate($text);
-                $response['text'] = $text;
-                $response['translate'] = $translate;
-                if ($needPopup) {
+                if ($needPopup || $showPopup) {
                     $response['popup'] = $this->renderView('translate/popup.html.twig', [
                         'text' => $text,
                         'translate' => $translate
                     ]);
                 }
+
+                $response['text'] = $text;
+                $response['translate'] = $translate;
             } catch (\Exception $e) {
                 $response['success'] = false;
                 $response['message'] = 'Exception: ' . $e->getMessage();
             }
         }
+
+        if ($showPopup && isset($response['popup'])) {
+            return new Response($response['popup']);
+        }
+
+        return new JsonResponse($response);
+    }
+
+    public function getIcon()
+    {
+        $response = [
+            'status' => true,
+            'icon' => [
+                'tag' => 'div',
+                'attributes' => [
+                    'id' => 'rayman_translate_icon',
+                    'style' => [
+                        'width' => '19px',
+                        'height' => '19px',
+                        'border' => '1px solid black',
+                        'position' => 'absolute !important',
+                        'border-radius' => '5px'
+                    ]
+                ],
+                'content' => $this->renderView('translate/icon.html.twig')
+            ]
+        ];
 
         return new JsonResponse($response);
     }
