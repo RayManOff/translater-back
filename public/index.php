a9782720c5866676f482192ba2c540905889d94e
rayman <raymanovg@gmail.com>
2018-10-14 14:03:35 +0300

    change to symfony

--- public/index.php
+++ public/index.php
@@ -1,114 +1,39 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+use App\Kernel;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
 
-use Slim\Container;
-use Slim\Http\Request;
-use Slim\Http\Response;
+require __DIR__.'/../vendor/autoload.php';
 
-$container = [
-    'settings' => [
-        'addContentLengthHeader' => false,
-        'displayErrorDetails' => true,
-        'google' => [
-            'access_token' => 'token'
-        ]
-    ],
-];
-
-$container['logger'] = function(Container $container) {
-    $logger = new \Monolog\Logger('translater');
-    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
-    $logger->pushHandler($file_handler);
-
-    return $logger;
-};
-
-$container['twig'] = function (Container $container) {
-    $view = new \Slim\Views\Twig('templates', [
-//        'cache' => '../cache/templates'
-    ]);
-
-    /** @var Request $request */
-    $request = $container->get('request');
-
-    $basePath = rtrim(str_ireplace('index.php', '', $request->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
-
-    return $view;
-};
-
-$app = new \Slim\App($container);
-
-/** @var \Monolog\Logger $logger */
-$logger = $app->getContainer()->get('logger');
-$logger->info('Starting app...');
-
-$app->get('/dialogContents', function (Request $request, Response $response) {
-    $this->logger->info('Response...');
-
-    /** @var \Slim\Views\Twig $twig */
-    $twig = $this->twig;
-    $result = [
-        'status' => true,
-        'dialogs' => [
-            'icon' => [
-                'tag' => 'div',
-                'attributes' => [
-                    'id' => 'custom_translate',
-                    'style' => [
-                        'width' => '19px',
-                        'height' => '19px',
-                        'border' => '1px solid black',
-                        'position' => 'absolute !important',
-                        'border-radius' => '5px'
-                    ]
-                ],
-                'content' => $twig->fetch('icon.html')
-            ],
-            'popup' => [
-                'tag' => 'div',
-                'attributes' => [
-                    'id' => 'translate_ext_rayman',
-                    'style' => [
-                        'width' => '100px',
-                        'height' => '100px',
-                        'border' => '1px solid black',
-                        'position' => 'absolute !important',
-                        'background-color' => 'white',
-                        'z-index' => 9999
-                    ]
-                ],
-            ]
-        ]
-    ];
-
-    /** @var \Slim\Views\Twig $twig */
-    return $response->withJson($result);
-});
-
-$app->post('/translate', function (Request $request, Response $response) {
-    $this->logger->info('Response...');
-    $result = [
-        'status' => true,
-        'translate_result' => []
-    ];
-
-    /** @var \Slim\Views\Twig $twig */
-    $twig = $this->twig;
-    $params = $request->getParsedBody();
-    if (empty($params['text'])) {
-        $result['status'] = false;
-        $result['error'] = 'There is not text to translate';
-    } else {
-        $result['data'] = [
-            'text' => 'Перевод текста " ' . $params['text'] . ' "',
-            'content' => $twig->fetch('popup.html', $params)
-        ];
+// The check is to ensure we don't use .env in production
+if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
+    if (!class_exists(Dotenv::class)) {
+        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
     }
+    (new Dotenv())->load(__DIR__.'/../.env');
+}
 
-    /** @var \Slim\Views\Twig $twig */
-    return $response->withJson($result);
-});
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
 
-$app->run();
+if ($debug) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts(explode(',', $trustedHosts));
+}
+
+$kernel = new Kernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
